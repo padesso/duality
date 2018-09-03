5d8daf08752ef5cec854a0edcab271f5c44aad30
Patrick Adesso <patrickadesso@gmail.com>
2018-09-03 13:42:58 -0400

    Created stubs for ICmptNetworkable and a networking component.

---
+++ Source/Core/Duality/Components/ICmpInterfaces/ICmpNetworkable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duality
+{
+	/// <summary>
+	/// Implement this interface in <see cref="Component">Components</see> that are considered networkable. 
+	/// </summary>
+	public interface ICmpNetworkable
+	{
+	}
+}
--- Source/Core/Duality/Components/Networking/NetworkingComponent.cs
+++ Source/Core/Duality/Components/Networking/NetworkingComponent.cs
@@ -3,11 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sockets.Plugin;
 
 namespace Duality.Components.Networking
 {
-	public class NetworkingComponent : Component
+	public class NetworkingComponent : Component, ICmpInitializable, ICmpUpdatable
 	{
-		[DontSerialize] public string IPAddress = "Test";
+		[DontSerialize] public string IPAddress = "127.0.0.1";
+		[DontSerialize] public int Port = 11011;
+		[DontSerialize] private UdpSocketClient client;
+
+		public void OnInit(InitContext context)
+		{
+			if(context == InitContext.Activate)
+			{
+				try
+				{
+					this.client = new UdpSocketClient();
+					this.client.ConnectAsync("127.0.0.1", this.Port);
+				}
+				catch (Exception ex)
+				{
+					Log.Core.WriteError("Error creating UDP Server: " + ex.Message);
+				}
+			}
+		}
+
+		public void OnShutdown(ShutdownContext context)
+		{
+			//TODO
+		}
+
+		public void OnUpdate()
+		{
+			byte[] sampleData = Encoding.UTF8.GetBytes("test data");
+			this.client.SendAsync(sampleData);
+		}
 	}
 }
